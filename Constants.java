4d97f772a7b3f53ccfeb108dbe869f1157ab5c31
TurzoMoniBala <turzo.math.relex.sy.404@gmail.com>
2024-11-27 23:34:48 -0500

    replese string veriable with Conostants and create a new class and short there

---
+++ Constants.java
@@ -0,0 +1,13 @@
+// Constants.java
+public class Constants {
+    // File path constants
+    public static final String EMPLOYEES_FILE_PATH = "employees.txt";
+    
+    // Other constants (if needed)
+    public static final String LOADING_DATA_MSG = "Loading data ...";
+    public static final String DATA_LOADED_MSG = "Data Loaded.";
+    public static final String EMPLOYEE_FOUND_MSG = "Employee found!";
+    public static final String ERROR_READING_FILE_MSG = "Error reading file.";
+    public static final String ERROR_WRITING_FILE_MSG = "Error writing to file.";
+    public static final String ERROR_INCORRECT_ARGS_MSG = "error: incorrect number";
+}
